631735b7a23a2101aaf24093a8b84c9f96923bad
Carifio24 <carifio24@gmail.com>
2022-12-06 16:33:47 -0500

    Add ability to let exported table be sorted by specified columns.

--- glue_plotly/html_exporters/table.py
+++ glue_plotly/html_exporters/table.py
@@ -9,8 +9,18 @@ from glue_plotly import PLOTLY_LOGO
 
 from qtpy import compat
 from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QDialog
 from plotly.offline import plot
 import plotly.graph_objs as go
+from pandas import DataFrame
+
+from .. import make_sortable
+
+try:
+    import dask.array as da
+    DASK_INSTALLED = True
+except ImportError:
+    DASK_INSTALLED = False
 
 
 @viewer_tool
@@ -20,17 +30,46 @@ class PlotlyTableExport(Tool):
     action_text = 'Save Plotly HTML page'
     tool_tip = 'Save Plotly HTML page'
 
-    def activate(self):
+    # We use this instead of self.viewer.model.data_for_row_and_column
+    # because we need the value (for sorting), and that method returns a string
+    def _data_for_cell(self, row, column):
+        model = self.viewer.model
+        c = model.columns[column]
+        idx = model.order_visible[row]
+        comp = model._data[c]
+        value = comp[idx]
+        if isinstance(value, bytes):
+            return value.decode('ascii')
+        else:
+            if DASK_INSTALLED and isinstance(value, da.Array):
+                return value.compute()
+            else:
+                return comp[idx]
 
-        filename, _ = compat.getsavefilename(parent=self.viewer, basedir="plot.html")
+    def activate(self):
 
         model = self.viewer.model
 
-        header = dict(values=[c.label for c in model.columns])
+        components = model.columns
+        column_names = [c.label for c in model.columns]
+        dialog = make_sortable.MakeSortableDialog(components=components)
+        result = dialog.exec_()
+        if result == QDialog.Rejected:
+            return
+
+        filename, _ = compat.getsavefilename(parent=self.viewer, basedir="plot.html")
+        if not filename:
+            return
+
+        sort_components = dialog.sort_components
+        header = dict(values=column_names)
         n_rows = model.rowCount()
         n_cols = model.columnCount()
 
-        data = [[model.data_by_row_and_column(row, col, Qt.DisplayRole) for row in range(n_rows)] for col in range(n_cols)]
+        data = [[self._data_for_cell(row, col) for row in range(n_rows)] for col in range(n_cols)]
+
+        # Keep as strings for formatting consistency with table viewer
+        cells = [[str(x) for x in col] for col in data]
 
         # Each row has a color, so we only need to get the data color for the first cell
         white = (256, 256, 256, 256)
@@ -41,7 +80,37 @@ class PlotlyTableExport(Tool):
             color = tuple(color[:3] + (color[3]/256,))
             colors.append('rgba{0}'.format(color))
 
-        table = go.Table(header=header, cells=dict(values=data, fill_color=[colors]))
+        table = go.Table(header=header, cells=dict(values=cells, fill_color=[colors]))
         fig = go.Figure(table)
+
+        buttons = []
+        df = DataFrame({c: data[i] for i, c in enumerate(column_names)})
+        for col in sort_components:
+            scores = df.sort_values(by=[col])
+            sort_colors = [colors[i] for i in scores.index]
+            button = dict(
+                label=col,
+                method="restyle",
+                args=[
+                    dict(
+                        cells=dict(
+                            values=[[str(x) for x in scores[c].values] for c in column_names],
+                            fill=dict(color=[sort_colors])
+                        )
+                    )
+                ],
+            )
+            buttons.append(button)
+
+        fig.update_layout(
+            updatemenus=[
+                dict(
+                    buttons=buttons,
+                    direction='down',
+                    pad={"r": 10, "t": 10},
+                    showactive=True,
+                    y=1
+                )
+        ])
 
         plot(fig, filename=filename, auto_open=False)
---
+++ glue_plotly/make_sortable.py
@@ -0,0 +1,97 @@
+import os
+
+from qtpy.QtWidgets import QDialog, QListWidgetItem
+from qtpy.QtCore import Qt
+
+from glue.external.echo import SelectionCallbackProperty
+from glue.external.echo.qt import autoconnect_callbacks_to_qt
+
+from glue.utils.qt import load_ui
+from glue.core.state_objects import State
+from glue.external.echo import ChoiceSeparator
+from glue.core.data_combo_helper import ComboHelper
+
+import numpy as np
+
+
+__all__ = ['MakeSortableDialog']
+
+
+class MakeSortableState(State):
+
+    component = SelectionCallbackProperty()
+
+    def __init__(self, components=component):
+
+        super(MakeSortableState, self).__init__()
+
+        self.component_helper = ComboHelper(self, 'component')
+        self.component_helper.choices = components
+
+
+class MakeSortableDialog(QDialog):
+
+    def __init__(self, components=None, parent=None):
+
+        super(MakeSortableDialog, self).__init__(parent=parent)
+
+        self.ui = load_ui('make_sortable.ui', self,
+                          directory=os.path.dirname(__file__))
+
+        self.state = MakeSortableState(components=components)
+
+        self._connections = autoconnect_callbacks_to_qt(self, self.ui)
+        self.sort_components = []
+
+        self.ui.button_cancel.clicked.connect(self.reject)
+        self.ui.button_ok.clicked.connect(self.accept)
+        self.ui.button_select_none.clicked.connect(self.select_none)
+        self.ui.button_select_all.clicked.connect(self.select_all)
+
+        self.ui.list_component.itemChanged.connect(self._on_check_change)
+
+        self._populate_list()
+
+    def _populate_list(self):
+
+        components = getattr(type(self.state), 'component').get_choices(self.state)
+        self.ui.list_component.clear()
+
+        for (component, k) in zip(components, np.arange(0, len(components))):
+
+            if isinstance(component, ChoiceSeparator):
+                item = QListWidgetItem(str(component))
+                item.setFlags(item.flags() & ~Qt.ItemIsSelectable)
+                item.setForeground(Qt.gray)
+            else:
+                item = QListWidgetItem(component.label)
+
+            item.setCheckState(Qt.Unchecked)
+            self.ui.list_component.addItem(item)
+
+    def _on_check_change(self, *event):
+
+        self.sort_components = []
+        for idx in range(self.ui.list_component.count()):
+            item = self.ui.list_component.item(idx)
+            if item.checkState() == Qt.Checked:
+                self.sort_components.append(item.text())
+
+    def select_none(self, *event):
+        self._set_all_checked(False)
+
+    def select_all(self, *event):
+        self._set_all_checked(True)
+
+    def _set_all_checked(self, check_state):
+        for idx in range(self.ui.list_component.count()):
+            item = self.ui.list_component.item(idx)
+            item.setCheckState(Qt.Checked if check_state else Qt.Unchecked)
+
+    def accept(self):
+        self.sort_components = []
+        for idx in range(self.ui.list_component.count()):
+            item = self.ui.list_component.item(idx)
+            if item.checkState() == Qt.Checked:
+                self.sort_components.append(item.text())
+        super(MakeSortableDialog, self).accept()
