e94192f84d063c68dcef8df27b3527afcde902d5
Jon Carifio <carifio24@gmail.com>
2023-07-29 02:02:50 -0400

    Keep the viewer state and axis bounds in sync.

--- glue_plotly/viewers/viewer.py
+++ glue_plotly/viewers/viewer.py
@@ -1,4 +1,6 @@
+from echo import delay_callback
 from glue.config import settings
+from glue_jupyter.link import link
 from glue_jupyter.view import IPyWidgetView
 
 import plotly.graph_objects as go
@@ -35,13 +37,17 @@ class PlotlyBaseView(IPyWidgetView):
 
         self.state.add_callback('x_axislabel', self.update_x_axislabel)
         self.state.add_callback('y_axislabel', self.update_y_axislabel)
-        self.state.add_callback('x_min', self._update_plotly_limits)
-        self.state.add_callback('x_max', self._update_plotly_limits)
-        self.state.add_callback('y_min', self._update_plotly_limits)
-        self.state.add_callback('y_max', self._update_plotly_limits)
+        self.state.add_callback('x_min', self._update_plotly_x_limits)
+        self.state.add_callback('x_max', self._update_plotly_x_limits)
+        self.state.add_callback('y_min', self._update_plotly_y_limits)
+        self.state.add_callback('y_max', self._update_plotly_y_limits)
         self.state.add_callback('show_axes', self._update_axes_visible)
 
-        self._update_plotly_limits()
+        self.axis_x.on_change(lambda _obj, x_range: self._set_x_state_bounds(x_range), 'range')
+        self.axis_y.on_change(lambda _obj, y_range: self._set_y_state_bounds(y_range), 'range')
+
+        self._update_plotly_x_limits()
+        self._update_plotly_y_limits()
         self._update_axes_visible()
 
         self.create_layout()
@@ -60,11 +66,17 @@ class PlotlyBaseView(IPyWidgetView):
     def update_y_axislabel(self, label):
         self.axis_y['title'] = label
 
-    def _update_plotly_limits(self, *args):
+    def _update_plotly_x_limits(self, *args):
+        print("_update_plotly_x_limits")
+        print(self.axis_x['range'])
         with self.figure.batch_update():
             if self.state.x_min is not None and self.state.x_max is not None:
                 self.axis_x['range'] = [self.state.x_min, self.state.x_max]
 
+    def _update_plotly_y_limits(self, *args):
+        print("_update_plotly_y_limits")
+        print(self.axis_y['range'])
+        with self.figure.batch_update():
             if self.state.y_min is not None and self.state.y_max is not None:
                 self.axis_y['range'] = [self.state.y_min, self.state.y_max]
 
@@ -72,6 +84,19 @@ class PlotlyBaseView(IPyWidgetView):
         with self.figure.batch_update():
             self.axis_x.visible = self.state.show_axes
             self.axis_y.visible = self.state.show_axes
+
+    def _set_x_state_bounds(self, x_range):
+        print("_set_x_state_bounds", x_range)
+        with delay_callback(self.state, 'x_min', 'x_max'):
+            self.state.x_min = x_range[0]
+            self.state.x_max = x_range[1]
+
+
+    def _set_y_state_bounds(self, y_range):
+        print("_set_y_state_bounds", y_range)
+        with delay_callback(self.state, 'y_min', 'y_max'):
+            self.state.y_min = y_range[0]
+            self.state.y_max = y_range[1]
 
     @property
     def figure_widget(self):
