16df595a66ff3897212edd3e0f0b66c993d98dd0
Carifio24 <carifio24@gmail.com>
2023-08-02 11:19:16 -0400

    Fix ROI application for histogram viewer.

--- glue_plotly/viewers/histogram/viewer.py
+++ glue_plotly/viewers/histogram/viewer.py
@@ -1,4 +1,4 @@
-from glue.core.subset import roi_to_subset_state
+from glue.core.subset import XRangeROI, roi_to_subset_state
 from glue.viewers.histogram.state import HistogramViewerState
 from glue_plotly.common import base_layout_config, base_rectilinear_axis
 from glue_plotly.viewers import PlotlyBaseView
@@ -38,7 +38,7 @@ class PlotlyHistogramView(PlotlyBaseView):
 
     def _create_layout_config(self):
         config = base_layout_config(self, barmode="overlay", bargap=0,
-                                    width=1200, height=600,
+                                    width=1000, height=600,
                                     **self.LAYOUT_SETTINGS)
         x_axis = base_rectilinear_axis(self.state, 'x')
         x_axis.update(showline=False)
@@ -58,3 +58,21 @@ class PlotlyHistogramView(PlotlyBaseView):
         
     def _roi_to_subset_state(self, roi):
         return roi_to_subset_state(roi, x_att=self.state.x_att)
+
+    def apply_roi(self, roi, override_mode=False):
+        if len(self.layers) == 0:
+            return
+
+        bins = self.state.bins
+
+        x = roi.to_polygon()[0]
+        lo, hi = min(x), max(x)
+
+        if lo >= bins.min():
+            lo = bins[bins <= lo].max()
+        if hi <= bins.max():
+            hi = bins[bins >= hi].min()
+
+        roi_new = XRangeROI(min=lo, max=hi)
+        subset_state = roi_to_subset_state(roi_new, x_att=self.state.x_att, x_categories=self.state.x_categories)
+        self.apply_subset_state(subset_state, override_mode=override_mode)
