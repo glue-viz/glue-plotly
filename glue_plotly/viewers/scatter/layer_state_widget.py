5e1fffbc69cd03eee50b10091c0ed3373e8f5603
Carifio24 <carifio24@gmail.com>
2025-01-06 11:57:35 -0500

    Use custom subclass of scatter layer widget for controlling border parameters.

---
+++ glue_plotly/viewers/scatter/layer_state_widget.py
@@ -0,0 +1,8 @@
+from glue_jupyter.common.state_widgets.layer_scatter import ScatterLayerStateWidget
+from traitlets import Bool
+
+
+class PlotlyScatterLayerStateWidget(ScatterLayerStateWidget):
+
+    template_file = (__file__, "layer_state_widget.vue")
+    border_color_menu_open = Bool(False).tag(sync=True)
---
+++ glue_plotly/viewers/scatter/layer_state_widget.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="glue-plotly-layer-scatter">
+        <div class="text-subtitle-2 font-weight-bold">Color</div>
+        <div>
+            <v-select label="color" :items="cmap_mode_items" v-model="cmap_mode_selected" hide-details />
+        </div>
+        <template v-if="glue_state.cmap_mode === 'Linear'">
+            <div>
+                <v-select label="attribute" :items="cmap_att_items" v-model="cmap_att_selected" hide-details />
+            </div>
+            <div>
+                <glue-float-field label="min" :value.sync="glue_state.cmap_vmin" />
+            </div>
+            <div>
+                <glue-float-field label="max" :value.sync="glue_state.cmap_vmax" />
+            </div>
+            <div>
+                <v-select label="colormap" :items="cmap_items" :value="glue_state.cmap" @change="set_colormap" hide-details/>
+            </div>
+        </template>
+        <div>
+            <v-subheader class="pl-0 slider-label">opacity</v-subheader>
+            <glue-throttled-slider wait="300" min="0" max="1" step="0.01" :value.sync="glue_state.alpha" hide-details />
+        </div>
+        <div class="text-subtitle-2 font-weight-bold">Points</div>
+        <div>
+            <v-subheader class="pl-0 slider-label">show points</v-subheader>
+            <v-switch v-model="glue_state.markers_visible" hide-details style="margin-top: 0" />
+        </div>
+        <template v-if="glue_state.markers_visible">
+            <div>
+                <v-select label="type" :items="points_mode_items" v-model="points_mode_selected" hide-details />
+            </div>
+            <div v-if="glue_state.density_map === false">
+                <v-select label="size" :items="size_mode_items" v-model="size_mode_selected" hide-details />
+            </div>
+            <template v-if="glue_state.size_mode === 'Linear'">
+                <div>
+                    <v-select label="attribute" :items="size_att_items" v-model="size_att_selected" hide-details />
+                </div>
+                <div>
+                    <glue-float-field label="min" :value.sync="glue_state.size_vmin" />
+                </div>
+                <div>
+                    <glue-float-field label="max" :value.sync="glue_state.size_vmax" />
+                </div>
+            </template>
+            <template v-if="glue_state.density_map">
+                <div>
+                    <v-subheader class="pl-0 slider-label">dpi</v-subheader>
+                    <glue-throttled-slider wait="300" min="12" max="144" step="1" :value.sync="dpi" hide-details />
+                </div>
+                <div>
+                    <v-subheader class="pl-0 slider-label">contrast</v-subheader>
+                    <glue-throttled-slider wait="300" min="0" max="1" step="0.01" :value.sync="glue_state.density_contrast"
+                                         hide-details />
+                </div>
+            </template>
+            <template v-else>
+                <div>
+                      <v-subheader class="pl-0 slider-label">fill markers</v-subheader>
+                      <v-switch v-model="glue_state.fill" hide-details style="margin-top: 0" />
+                </div>
+                <div>
+                      <v-subheader class="pl-0 slider-label">show borders</v-subheader>
+                      <v-switch v-model="glue_state.border_visible" hide-details style="margin-top: 0" />
+                </div>
+                <div>
+                    <v-subheader class="pl-0 slider-label">border size</v-subheader>
+                    <glue-throttled-slider wait="300" min="0" max="10" step="1" :value.sync="glue_state.border_size"
+                        hide-details />
+                </div>
+                <div>
+                    <v-subheader class="pl-0 slider-label">border color</v-subheader>
+                    <v-menu v-model="border_color_menu_open">
+                        <template v-slot:activator="{ on }">
+                            <span class="glue-color-menu"
+                                  :style="`background: ${glue_state.border_color}`"
+                                  @click.stop="on.click"
+                            >&nbsp;</span>
+                        </template>
+                        <div @click.stop="" style="text-align: end; background-color: white">
+                            <v-btn icon @click="border_color_menu_open = false">
+                                <v-icon>mdi-close</v-icon>
+                            </v-btn>
+                            <v-color-picker v-model="glue_state.border_color"></v-color-picker>
+                        </div>
+                    </v-menu>
+                </div>
+                <div>
+                    <v-subheader class="pl-0 slider-label">size scaling</v-subheader>
+                    <glue-throttled-slider wait="300" min="0.1" max="10" step="0.01" :value.sync="glue_state.size_scaling"
+                        hide-details />
+                </div>
+            </template>
+        </template>
+        <div class="text-subtitle-2 font-weight-bold" :style="glue_state.markers_visible ? {} : {marginTop: '6px'}">Line</div>
+        <div>
+            <v-subheader class="pl-0 slider-label">show line</v-subheader>
+            <v-switch v-model="glue_state.line_visible" hide-details style="margin-top: 0"/>
+        </div>
+        <template v-if="glue_state.line_visible">
+            <div>
+                <v-subheader class="pl-0 slider-label">width</v-subheader>
+                <glue-throttled-slider wait="300" min="1" max="20" step="1" :value.sync="glue_state.linewidth" hide-details />
+            </div>
+            <div>
+                <v-select label="linestyle" :items="linestyle_items" v-model="linestyle_selected" hide-details />
+            </div>
+        </template>
+        <div class="text-subtitle-2 font-weight-bold" :style="glue_state.markers_visible ? {} : {marginTop: '6px'}">Vectors</div>
+        <div>
+            <v-subheader class="pl-0 slider-label">show vectors</v-subheader>
+            <v-switch v-model="glue_state.vector_visible" hide-details style="margin-top: 0"/>
+        </div>
+        <template v-if="glue_state.vector_visible">
+            <div>
+                <v-select label="vx" :items="vx_att_items" v-model="vx_att_selected" hide-details />
+            </div>
+            <div>
+                <v-select label="vy" :items="vy_att_items" v-model="vy_att_selected" hide-details />
+            </div>
+        </template>
+    </div>
+</template>
+<script>
+</script>
+<style id="layer_scatter">
+.glue-plotly-layer-scatter .v-subheader.slider-label {
+    font-size: 12px;
+    height: 16px;
+    margin-top: 6px;
+}
+</style>
